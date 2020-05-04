73b3d76a97844167e0a21f4605ad27336117a93f
Amir <toamir32@gmail.com>
2020-05-04 12:29:15 +0500

    Dotnet Report Plugin

---
+++ src/Libraries/Nop.Services/DotnetReports/IDotnetReports.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Services.Plugins;
+
+namespace Nop.Services.DotnetReports
+{
+   public interface IDotnetReports : IPlugin
+    {
+    }
+}
--- src/Plugins/Nop.Plugin.Reports.DotnetReport/DotnetReportPlugin.cs
+++ src/Plugins/Nop.Plugin.Reports.DotnetReport/DotnetReportPlugin.cs
@@ -1,10 +1,57 @@
-﻿using System;
+﻿
+using System;
+using Nop.Core;
+using Nop.Services.DotnetReports;
 using Nop.Services.Plugins;
 
 namespace Nop.Plugin.Reports.DotnetReport
 {
-    public class DotnetReportPlugin : BasePlugin
+    public class DotnetReportPlugin : BasePlugin, IDotnetReports
     {
+        #region Fields
 
+        private readonly IWebHelper _webHelper;
+
+        #endregion
+        #region Ctor
+
+        public DotnetReportPlugin(
+            IWebHelper webHelper)
+        {
+            _webHelper = webHelper;
+        }
+        #endregion
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/Report/Index";
+        }
+
+        /// <summary>
+        /// Install the plugin
+        /// </summary>
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        /// <summary>
+        /// Uninstall the plugin
+        /// </summary>
+        public override void Uninstall()
+        {
+
+            base.Uninstall();
+        }
     }
 }
+
+//using System;
+//using Nop.Services.Plugins;
+
+//namespace Nop.Plugin.Reports.DotnetReport
+//{
+//    public class DotnetReportPlugin : BasePlugin
+//    {
+
+//    }
+//}
